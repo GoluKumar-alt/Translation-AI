f22cda36dd9010895d044700e8e400df10e71235
GoluKumar-alt <devineetu414@gmail.com>
2026-04-18 09:45:57 +0530

    Refactor root route handler for clarity

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+JavaScript
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("App Live 🚀"));
+app.get("/", (req, res) => {
+  res.send("App Live 🚀");
+});
 
 app.listen(process.env.PORT || 3000);
