cabb589656d96a7bdcf15b7a8039c1e9a8287307
GoluKumar-alt <devineetu414@gmail.com>
2026-04-18 10:03:05 +0530

    Update response message to version 2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("App Live 🚀");
+  res.send("App Live version 2 🚀");
 });
 
 app.listen(process.env.PORT || 3000);
